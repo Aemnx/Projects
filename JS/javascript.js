1e27ff05291690d6eb55ea1d8e52dc5d6a726bca
K <70831713+Aemnx@users.noreply.github.com>
2026-03-29 14:10:47 +0800

    Testing JS

---
+++ JS/javascript.js
@@ -0,0 +1,26 @@
+let firstname = "John";
+let lastname = "doe";
+
+console.log(firstname);
+console.log(lastname);
+
+let age = "79";
+console.log(age);
+age = "54";
+console.log(age);
+
+const pie = 3.14;
+console.log(pie);
+
+console.log(23 + 97 + 10 + 9 + 11 + 20);
+console.log((4 + 6 + 9) / 77);
+let a = 10;
+console.log(a);
+a = 20;
+console.log(a);
+let b = 7 * a;
+console.log(b);
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
